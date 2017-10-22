de014373d302daca844513507dbc7c47166c4a7d
Brian Williams <williamsbm2000@comcast.net>
2017-10-22 10:43:32 -0700

    - added icons - added a couple more intersection locations - cleanup UI - added map and location - vibrate to indicate activity - auto-poll server within distance threshold

--- Traffic Buddy/Controllers/ViewController.swift
+++ Traffic Buddy/Controllers/ViewController.swift
@@ -6,19 +6,23 @@
 //  Copyright © 2017 Brian Williams. All rights reserved.
 //
 
+import AudioToolbox
 import CoreLocation
 import MapKit
 import UIKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     var intersections = [Intersection]()
+    var nearIntersection = false
     let locationManager = CLLocationManager()
+    let distanceThreshold = 1320.0 // quarter mile
     
     @IBOutlet var mainBackground: UIView!
-    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var nearestIntersectionLabel: UILabel!
     @IBOutlet weak var toggleSwitch: UISwitch!
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,15 +39,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
         }
         
+        intersections.append(Intersection(lat: 44.084221, long: -123.061607, name: "Cambridge Oaks Dr"))
         intersections.append(Intersection(lat: 44.080277, long: -123.067722, name: "Coburg & Willakenzie"))
         intersections.append(Intersection(lat: 44.040030, long: -123.080198, name: "18th & Alder"))
+        intersections.append(Intersection(lat: 44.045489, long: -123.070931, name: "13th Ave Kiosk"))
         intersections.append(Intersection(lat: 44.045689, long: -123.066324, name: "13th & Franklin"))
         intersections.append(Intersection(lat: 44.056741, long: -123.024210, name: "Centennial & Pioneer Pkwy W"))
         intersections.append(Intersection(lat: 44.056656, long: -123.023835, name: "Centennial & Pioneer Pkwy E"))
-        
-        countLabel.text = String(intersections.count)
     }
     
+    func metersToFeet(from: Double) -> Double {
+        return from * 3.28084
+    }
+
     func closestLocation(locations: [CLLocation], closestToLocation location: CLLocation) -> CLLocation? {
         if let closestLocation = locations.min(by: { location.distance(from: $0) < location.distance(from: $1) }) {
             return closestLocation
@@ -68,10 +76,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let locValue:CLLocationCoordinate2D = manager.location!.coordinate
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
         
-        locationLabel.text = "location: \(locValue.latitude) \(locValue.longitude)"
+        locationLabel.text = "Current Location:\n  \(locValue.latitude) \(locValue.longitude)"
         
+        let center = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
         displayClosestIntersection()
     }
     
@@ -91,18 +101,39 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
             
             let nearest = closestLocation(locations: coords, closestToLocation: locationManager.location!)
-            for i in (0...intersections.count-1) {
-                if intersections[i].location == nearest {
-                    nearestIntersectionLabel.text = "closest intersection: \(intersections[i].title), distance: \(String(describing: nearest!.distance(from: locationManager.location!))) meters"
-                    break
+            if nearest != nil {
+                let dist = metersToFeet(from: nearest!.distance(from: locationManager.location!))
+                
+                if dist < distanceThreshold {
+                    // auto-poll server within quarter mile
+                    pollServer()
+
+                    // if the user is not already near an intersection, vibrate to notify
+                    if dist < 50.0 && !nearIntersection {
+                        nearIntersection = true
+                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                    }
+                    
+                    // if the user was previously near an intersection, vibrate to notify
+                    if dist > 50.0 && nearIntersection {
+                        nearIntersection = false
+                        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+                    }
+                } else {
+                    mainBackground.backgroundColor = UIColor.white
+                }
+
+                for i in (0...intersections.count-1) {
+                    if intersections[i].location == nearest {
+                        nearestIntersectionLabel.text = "Nearest Intersection:\n  \(intersections[i].title)\nDistance:\n  \(String(describing: dist)) feet"
+                        break
+                    }
                 }
             }
         }
     }
-
-    @IBAction func pollServerButton(_ sender: Any) {
-        displayClosestIntersection()
-
+    
+    func pollServer() {
         if let url = URL(string: "https://dubflask.herokuapp.com") {
             do {
                 let contents = try String(contentsOf: url)
@@ -112,14 +143,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 else {
                     mainBackground.backgroundColor = UIColor.red
                 }
-                countLabel.text = contents
+                infoLabel.text = contents
             } catch {
                 // contents could not be loaded
-                countLabel.text = "couldn't load url"
+                infoLabel.text = "couldn't load url"
             }
         } else {
             // the URL was bad!
         }
+    }
+
+    @IBAction func pollServerButton(_ sender: Any) {
+        pollServer()
     }
 }
 

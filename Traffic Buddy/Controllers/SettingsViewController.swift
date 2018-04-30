74b9b22b1d1775f536dd373f36ea8946515dbd77
Brian Williams <williamsbm2000@comcast.net>
2018-04-29 21:35:25 -0700

    added new settings view controller

--- Traffic Buddy/Controllers/SettingsViewController.swift
+++ Traffic Buddy/Controllers/SettingsViewController.swift
@@ -9,6 +9,79 @@
 import Foundation
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
+    @IBOutlet weak var distanceFeetSlider: UISlider!
+    @IBOutlet weak var distanceFeetLabel: UILabel!
+    @IBOutlet weak var distanceSecondsSlider: UISlider!
+    @IBOutlet weak var distanceSecondsLabel: UILabel!
+    @IBOutlet weak var sensorChangeStepper: UIStepper!
+    @IBOutlet weak var sensorChangeLabel: UILabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        // load config info
+        if let configs = RealmHelper.sharedInstance.getObjects(type: ConfigItem.self) {
+            for item in configs {
+                if let config = item as? ConfigItem {
+                    if config.key == "SensorChangeThreshold" {
+                        if let val = Double(config.value) {
+                            print("sensor change threshold \(val)")
+                            sensorChangeStepper.value = val
+                        }
+                        sensorChangeLabel.text = "\(sensorChangeStepper.value)"
+                    }
+                    else if config.key == "DistanceThresholdFeet" {
+                        if let val = Float(config.value) {
+                            print("threshold distance \(val)")
+                            distanceFeetSlider.value = val
+                        }
+                        distanceFeetLabel.text = "\(distanceFeetSlider.value) feet"
+                    }
+                    else if config.key == "DistanceThresholdSeconds" {
+                        if let val = Float(config.value) {
+                            print("threshold seconds \(val)")
+                            distanceSecondsSlider.value = val
+                        }
+                        distanceSecondsLabel.text = "\(distanceSecondsSlider.value) feet"
+                    }
+                    else {
+                        print("unknown key: '\(config.key)'")
+                    }
+                }
+            }
+        }
+        super.viewWillAppear(animated)
+    }
+    
+    @IBAction func distanceFeetChanged(_ sender: Any) {
+        distanceFeetLabel.text = "\(distanceFeetSlider.value) feet"
+        if let distanceFeet = RealmHelper.sharedInstance.getObjects(type: ConfigItem.self)?.filter("key = 'DistanceThresholdFeet'")[0] as? ConfigItem {
+            let copy = ConfigItem()
+            copy.id = distanceFeet.id
+            copy.key = "DistanceThresholdFeet"
+            copy.value = "\(distanceFeetSlider.value)"
+            RealmHelper.sharedInstance.editObject(obj: copy)
+        }
+    }
+    
+    @IBAction func distanceSecondsChanged(_ sender: Any) {
+        distanceSecondsLabel.text = "\(distanceSecondsSlider.value) seconds"
+        if let distanceSeconds = RealmHelper.sharedInstance.getObjects(type: ConfigItem.self)?.filter("key = 'DistanceThresholdSeconds'")[0] as? ConfigItem {
+            let copy = ConfigItem()
+            copy.id = distanceSeconds.id
+            copy.key = "DistanceThresholdSeconds"
+            copy.value = "\(distanceSecondsSlider.value)"
+            RealmHelper.sharedInstance.editObject(obj: copy)
+        }
+    }
+    
+    @IBAction func sensorDistanceThresholdChanged(_ sender: Any) {
+        sensorChangeLabel.text = "\(sensorChangeStepper.value)"
+        if let sensorChangeThreshold = RealmHelper.sharedInstance.getObjects(type: ConfigItem.self)?.filter("key = 'SensorChangeThreshold'")[0] as? ConfigItem {
+            let copy = ConfigItem()
+            copy.id = sensorChangeThreshold.id
+            copy.key = "SensorChangeThreshold"
+            copy.value = "\(sensorChangeStepper.value)"
+            RealmHelper.sharedInstance.editObject(obj: copy)
+        }
+    }
 }
